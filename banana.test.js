242203b9fb6105cd39608a2fc1f5f3532c9a6a92
Lee Warrick <leewarrick@gmail.com>
2020-02-28 15:56:14 -0500

    get basic tests up

--- banana.test.js
+++ banana.test.js
@@ -1,22 +1,39 @@
-describe('Google', () => {
+// describe('Google', () => {
+//   beforeAll(async () => {
+//     const context = await browser.newContext({
+//       userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36",
+//       viewport: {
+//         width: 1920,
+//         height: 1080
+//       }
+//     })
+//     page = await context.newPage()
+//     await page.goto('https://whatismybrowser.com/')
+//   })
+
+//   it('should click and go to another page', async () => {
+//     await Promise.all([
+//       page.waitForNavigation(),
+//       page.click('.label-holder a'),
+//     ])
+//     const title = await page.$eval('#main > section > article > div > h1', el => el.textContent)
+//     expect(title.includes('Update')).toBe(true)
+//   })
+// })
+
+describe('realtruck', () => {
   beforeAll(async () => {
-    await page.setViewportSize({
-      width: 1920, height: 1080,
-    });
-    await page.goto('https://whatismybrowser.com/')
+    await page.setViewportSize({width: 1920, height: 1080})
+    await page.goto('https://realtruck.com/')
   })
 
-  // it('should display "google" text on page', async () => {
-  //   const browser = await page.$eval('.string-major a', el => el.text)
-  //   expect(browser).toContain('Chrome')
-  // })
-
-  it('should click and go to another page', async () => {
+  it('should click tonneau covers', async () => {
     await Promise.all([
       page.waitForNavigation(),
-      page.click('.label-holder a'),
+      page.click('#app > div.homepage-content > div.section.shop-category > div > div > div:nth-child(2) > a')
     ])
-    const title = await page.$eval('h1', el => el.text)
-    expect(title.includes('Update')).toBe(true)
+    await page.waitForSelector('#catalog-title > span')
+    const header = await page.$eval('#catalog-title > span', el => el.textContent)
+    expect(header).toBe('Tonneau Covers')
   })
 })
